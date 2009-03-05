29805d513bb6f0ef9a4cf2f7bdb264e5da337648
Zack Cerza <zack@cerza.org>
2009-03-04 19:47:43 -0500

    'si' changed name to 'linux_icon'

---
+++ linux_notifier.rb
@@ -0,0 +1,37 @@
+require 'notifier'
+require 'linux_icon'
+
+class LinuxNotifier < Notifier
+    def initialize
+        super
+        @icon = LMGtkIcon.new
+    end
+
+    def notify(msgs)
+        body = ""
+        tooltip = ""
+        msgs.each do |msg|
+            msg_s = "#{msg.to_s}\n\n"
+            tooltip += msg_s
+            if not @seen.member? msg.uid
+                body += msg_s
+                @seen << msg.uid
+            end
+        end
+        body.strip!
+        tooltip.strip!
+        @icon.notify tooltip
+        return if body.length == 0
+        body.gsub!(/&/,"&amp;")
+        body.gsub!(/"/,"&quot;")
+        body.gsub!(/</,"&lt;")
+        body.gsub!(/>/,"&gt;")
+        system("notify-send", 
+               "-c", "email.arrived",
+               "-i", "mail-unread", 
+               "-t", "5000", 
+               @@title, body)
+    end
+end
+
+Gtk.init
